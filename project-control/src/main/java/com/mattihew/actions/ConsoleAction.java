2d1d8aaf4861d47739a45fb54f8958f4fa5ab91d
Matt Rayner <me@mattihew.com>
2018-04-19 16:19:27 +0100

    #1 created ConsoleAction

---
+++ project-control/src/main/java/com/mattihew/actions/ConsoleAction.java
@@ -0,0 +1,26 @@
+package com.mattihew.actions;
+
+import java.io.PrintStream;
+
+public class ConsoleAction implements Action
+{
+    private final PrintStream stream;
+    private final String msg;
+
+    public ConsoleAction(final String msg)
+    {
+        this(msg, System.out);
+    }
+
+    public ConsoleAction(final String msg, final PrintStream stream)
+    {
+        this.stream = stream;
+        this.msg = msg;
+    }
+
+    @Override
+    public void trigger()
+    {
+        this.stream.print(this.msg);
+    }
+}
