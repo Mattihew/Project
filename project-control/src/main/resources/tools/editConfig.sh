d44565add6b43f965d461014cd257ecbf1ac5933
Matt Rayner <me@mattihew.com>
2018-04-28 17:39:39 +0100

    updated configs

--- project-control/src/main/resources/tools/editConfig.sh
+++ project-control/src/main/resources/tools/editConfig.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
-file="config.properties"
-fileType="sh"
+COL="\e[4;96m"
+NC="\e[0m"
 jarFile="project-control.jar"
-jar -xvf ${jarFile} ${file}
-nano -ltY ${fileType} ${file}
-jar -uvf ${jarFile} ${file}
-rm -f ${file}
+choice=$1
+while [ "${choice}" != "c" ] && [ "${choice}" != "t" ]
+do
+    echo "which file to edit?"
+    echo -e "${COL}c${NC}: config.properties"
+    echo -e "${COL}t${NC}: triggers.json"
+    echo -en "choice: ${COL}"
+    read -n 1 choice
+    echo -e "${NC}"
+done
+
+if [ "${choice}" = "c" ]
+then
+    file="config.properties"
+    fileType="sh"
+elif [ "${choice}" = "t" ]
+then
+    file="triggers.json"
+    fileType="json"
+fi
+jar -xvf ${jarFile} "${file}"
+nano -ltY ${fileType} "${file}"
+jar -uvf ${jarFile} "${file}"
+rm -f "${file}"
